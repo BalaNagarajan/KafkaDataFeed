de59af1013ae21eff0edc22894a319b8960f2520
Bala <bala_tsm@yahoo.com>
2020-03-09 21:20:49 -0500

    Initial Changes

---
+++ src/main/java/com/jcircle/kafka/feed/KafkaDataFeedApplication.java
@@ -0,0 +1,13 @@
+package com.jcircle.kafka.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDataFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDataFeedApplication.class, args);
+	}
+
+}
